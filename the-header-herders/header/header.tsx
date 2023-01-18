ed03d6aead63429eb60dd9094c9999144f911639
Nitsan Cohen <77798308+NitsanCohen770@users.noreply.github.com>
2023-01-18 15:13:22 +0200

    remove links

--- the-header-herders/header/header.tsx
+++ the-header-herders/header/header.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { menuContent } from '@teambit/design.ui.navigation.menu';
 import { Logo } from '@teambit/design.ui.brand.logo';
 import { Header as BaseHeader } from '@teambit/design.blocks.header';
 
@@ -7,7 +6,6 @@ export const Header = () => {
   return (
     <BaseHeader
       data-testid="header"
-      menuLinks={menuContent}
       logo={
         <a href="https://bit.cloud">
           <Logo />
