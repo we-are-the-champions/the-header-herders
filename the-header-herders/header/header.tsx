324cec901b48c73068db52ad276eac4dac51019f
Nitsan Cohen <77798308+NitsanCohen770@users.noreply.github.com>
2023-01-19 21:51:50 +0200

    update header

--- the-header-herders/header/header.tsx
+++ the-header-herders/header/header.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { menuContent } from '@teambit/design.ui.navigation.menu';
 import { Logo } from '@teambit/design.ui.brand.logo';
 import { Header as BaseHeader } from '@teambit/design.blocks.header';
 
@@ -7,7 +6,15 @@ export const Header = () => {
   return (
     <BaseHeader
       data-testid="header"
-      menuLinks={menuContent}
+      plugins={[
+        <div>
+          {' '}
+          I am the header component being herd by{' '}
+          <a href="https://bit.cloud/we-are-the-champions/the-header-herders">
+            the header herders team
+          </a>
+        </div>,
+      ]}
       logo={
         <a href="https://bit.cloud">
           <Logo />
